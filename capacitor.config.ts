eda2fee1999aaaa1f6c62e4ca4fe13929bd407cc
Philip Cesar Garay <philipgaray2@gmail.com>
2022-07-26 11:06:53 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic_react_voyaga',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
